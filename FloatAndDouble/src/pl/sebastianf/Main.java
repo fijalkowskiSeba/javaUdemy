177c99973e320c2a024873a1d1e30abff1aeed0e
fijalkowskiSeba <fijalkowskiweb@gmail.com>
2022-03-07 22:56:56 +0100

    float and double range

---
+++ FloatAndDouble/src/pl/sebastianf/Main.java
@@ -0,0 +1,23 @@
+package pl.sebastianf;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        float floatMaxValue=Float.MAX_VALUE;
+        float floatMinValue=Float.MIN_VALUE;
+        System.out.println("Float min value: "+floatMinValue);
+        System.out.println("Float max value: "+floatMaxValue);
+
+        double doubleMaxValue=Double.MAX_VALUE;
+        double doubleMinValue=Double.MIN_VALUE;
+        System.out.println("Double min value: "+doubleMinValue);
+        System.out.println("Double max value: "+doubleMaxValue);
+
+        int myIntValue=5;
+        float myFloatValue=5.25f;
+        double myDoubleValue=5d;
+
+
+    }
+}
