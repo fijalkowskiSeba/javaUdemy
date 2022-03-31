5961acc6d59bd04478a2d63ff82e1f919988b1f8
fijalkowskiSeba <fijalkowskiweb@gmail.com>
2022-03-31 12:19:09 +0200

    Area Calculator

---
+++ AreaCalculator/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Pole kola o promieniu 5 : "+AreaCalculator.area(5));
+        System.out.println("Pole prostokata x=5 y=4 : "+AreaCalculator.area(5,4));
+    }
+}
