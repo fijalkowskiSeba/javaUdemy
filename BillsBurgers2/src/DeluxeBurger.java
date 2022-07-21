ed483f26c9e080dc8621a7fc10558ac111bcb7ba
fijalkowskiSeba <fijalkowskiweb@gmail.com>
2022-07-21 13:13:49 +0200

    Bills 2

---
+++ BillsBurgers2/src/DeluxeBurger.java
@@ -0,0 +1,29 @@
+public class DeluxeBurger extends Hamburger{
+    public DeluxeBurger(){
+        super("Deluxe","beef",10,"white");
+        super.addHamburgerAddition1("Chips",9);
+        super.addHamburgerAddition2("Drinks", 0.10);
+    }
+
+    @Override
+    public void addHamburgerAddition1(String name, double price) {
+        System.out.println("Cannot not add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+        System.out.println("Cannot not add additional items to a deluxe burger");
+
+    }
+
+    @Override
+    public void addHamburgerAddition3(String name, double price) {
+        System.out.println("Cannot not add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        System.out.println("Cannot not add additional items to a deluxe burger");
+    }
+
+}
